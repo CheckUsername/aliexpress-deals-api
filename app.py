307f70431a30ddd9199f744c1d85b92e6e3be0f1
CheckUsername <rororo19989@gmail.com>
2025-08-29 00:53:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,27 @@
 from flask import Flask, request, jsonify
-import requests, hashlib, time
-
-APP_KEY  = "509112"
-APP_SECRET = "UOXRatcL8CkX5d4ruigBcOWI7RObnkPz"
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def deal():
-    query = request.args.get("query", "")
+    query = request.args.get("query", "wireless earbuds")
+    url = "https://portals.aliexpress.com/affiliate/product/search"
     params = {
-        "app_key": APP_KEY,
-        "method": "aliexpress.affiliate.product.query",
         "keywords": query,
-        "page_no": 1,
-        "page_size": 1,
-        "target_currency": "USD",
-        "target_language": "EN",
-        "timestamp": str(int(time.time())),
-        "v": "2.0",
-        "format": "json"
+        "page": 1,
+        "size": 1,
+        "currency": "USD",
+        "lang": "en",
+        "affiliate_id": "YOUR_AFFILIATE_ID"  # استبدله بـ ID حسابك
     }
-    sorted_str = APP_SECRET + ''.join([k + str(v) for k, v in sorted(params.items())]) + APP_SECRET
-    params["sign"] = hashlib.md5(sorted_str.encode()).hexdigest().upper()
-    res = requests.get("https://gw.api.taobao.com/router/rest", params=params).json()
-    p = res["aliexpress_affiliate_product_query_response"]["resp_result"]["result"]["products"][0]
+    res = requests.get(url, params=params).json()
+    p = res["products"][0]
     return jsonify({
         "title": p["product_title"],
-        "image": p["product_main_image_url"],
+        "image": p["product_image"],
         "price_after": p["sale_price"],
-        "coupon_code": p.get("discount_coupon", {}).get("coupon_code", ""),
+        "coupon_code": p.get("coupon_code", ""),
         "affiliate_link": p["affiliate_link"]
     })
 
